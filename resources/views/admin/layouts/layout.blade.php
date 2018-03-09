0771583a88f6fd889a307ecc7d030d06c18cd35c
Justin Furnas <jfurnas@cenmi.org>
2018-03-09 11:07:44 -0500

    Made some more minor changes to get the boilerplate ready

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -25,31 +25,31 @@
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link active" href="/admin/">
                   <span data-feather="home"></span>
                   Dashboard <span class="sr-only">(current)</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="accounts.html">
+                <a class="nav-link" href="/accounts/">
                   <span data-feather="users"></span>
                   Accounts
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="pages.html">
+                <a class="nav-link" href="/pages/">
                   <span data-feather="file"></span>
                   Pages
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="support.html">
+                <a class="nav-link" href="/support/">
                   <span data-feather="help-circle"></span>
                   Support Tickets
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="settings.html">
+                <a class="nav-link" href="/settings/">
                   <span data-feather="settings"></span>
                   Settings
                 </a>
--- resources/views/admin/layouts/usernav.blade.php
+++ resources/views/admin/layouts/usernav.blade.php
@@ -1,6 +1,10 @@
 	<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
 		<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">TCWCMS</a>
 		<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+		<select class="form-control" name="realm">
+			<option>Test Realm 1</option>
+			<option>Test Realm 2</option>
+		</select>
 	    <ul class="navbar-nav px-3">
 	    	<li class="nav-item text-nowrap">
 	        	<a class="nav-link" href="#">Sign out</a>
